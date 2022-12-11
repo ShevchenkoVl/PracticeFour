d8c09779ffaee6eeb2c556b924d3f43efaa269de
ShevchenkoVl <7valentin7777@gmail.com>
2022-12-11 04:36:45 +0200

    PracticeFour

---
+++ app/src/main/java/com/practice4/secondActivity.kt
@@ -0,0 +1,11 @@
+package com.practice4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class secondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
